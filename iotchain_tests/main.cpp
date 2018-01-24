6437a5fc1511a76ba824df34b78b6462721236e9
tanisman <mail@sezercantanisman.com.tr>
2018-01-24 02:57:55 +0200

    acceptor changes

--- iotchain_tests/main.cpp
+++ iotchain_tests/main.cpp
@@ -28,6 +28,8 @@ int main(int argc, char **argv)
 		std::cout << "Test case '" << p2p_test::TEST_NAME << "' has succeeded" << std::endl;
 	}
 #endif //RUN_P2P_TEST
+#if defined (WIN32)
 	system("pause");
+#endif
 	return 0;
 }
